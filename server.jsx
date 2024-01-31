e2ad65400302d121bad4a3d873c5ba5b6325600d
Madhumitha <madhumithavenu03@gmail.com>
2024-01-31 13:29:02 +0530

    Connecting to database

--- server.jsx
+++ server.jsx
@@ -1,6 +1,15 @@
+const dotenv = require('dotenv');
 const express = require('express');
+const  mongoose = require('mongoose');
 const app = express()
-const port = 5000
 
-app.get('/', (req, res) => res.send('Hello world'))
-app.listen(port, ()=>console.log(`Example App on ${port}`));
+
+
+// app.get('/', (req, res) => res.send('Hello world'))
+// app.listen(port, ()=>console.log(`Example App on ${port}`));
+
+dotenv.config();
+
+mongoose.connect(`mongodb+srv://madhu:${process.env.PASSWORD}@cluster0.sqb10q8.mongodb.net/?retryWrites=true&w=majority`)
+    .then(app.listen(process.env.PORT))
+    .then(() => { console.log(`DB Connect & App Started at ${process.env.PORT}`) });
