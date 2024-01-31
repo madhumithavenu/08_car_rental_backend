51fe36ecac5b0334f20e40561837086a55c1ab85
Madhumitha <madhumithavenu03@gmail.com>
2024-01-31 14:40:23 +0530

    Creating car model

--- server.jsx
+++ server.jsx
@@ -5,11 +5,10 @@ const app = express()
 
 
 
-// app.get('/', (req, res) => res.send('Hello world'))
-// app.listen(port, ()=>console.log(`Example App on ${port}`));
+app.get('/', (req, res) => res.send('Hello world'))
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://madhu:${process.env.PASSWORD}@cluster0.sqb10q8.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://madhu:${process.env.PASSWORD}@cluster0.sqb10q8.mongodb.net/cars?retryWrites=true&w=majority`)
     .then(app.listen(process.env.PORT))
     .then(() => { console.log(`DB Connect & App Started at ${process.env.PORT}`) });
