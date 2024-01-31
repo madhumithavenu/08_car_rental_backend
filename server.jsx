f65117da0d870b54599c5cf174cea0a92160e05f
Madhumitha <madhumithavenu03@gmail.com>
2024-01-31 15:11:40 +0530

    Add static data to cars collection

--- server.jsx
+++ server.jsx
@@ -9,6 +9,6 @@ app.get('/', (req, res) => res.send('Hello world'))
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://madhu:${process.env.PASSWORD}@cluster0.sqb10q8.mongodb.net/cars?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://madhu:${process.env.PASSWORD}@cluster0.sqb10q8.mongodb.net/cars`)
     .then(app.listen(process.env.PORT))
     .then(() => { console.log(`DB Connect & App Started at ${process.env.PORT}`) });
